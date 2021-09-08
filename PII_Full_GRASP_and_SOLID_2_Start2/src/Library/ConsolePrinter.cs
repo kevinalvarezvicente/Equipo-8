16d6df20f25134a0dfa9032e50f647a3ceb4f6c6
kevinalvarezvicente <64856845+kevinalvarezvicente@users.noreply.github.com>
2021-09-08 11:13:43 -0300

    Fix

---
+++ PII_Full_GRASP_and_SOLID_2_Start2/src/Library/ConsolePrinter.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class ConsolePrinter
+    {
+
+        public void PrintRecipe1(String recipe)
+        {
+            Console.WriteLine(recipe);
+        }
+    }
+}
